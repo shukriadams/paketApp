84677fca43794f4249466a6c729ba9ebb3952d7a
Shukri Adams <shukri.adams@gmail.com>
2021-03-28 01:19:39 +0100

    moving in

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SimpleLib;
+using ComplexLib;
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var simple = Simple.DoSomething();
+            Complex complex = new Complex();
+            var complexthing = complex.DoComplex();
+            Console.WriteLine($"Hello World! simple : {simple} ... complex {complexthing}");
+        }
+    }
+}
